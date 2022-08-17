7781c6db8e79adbc6e6ce2475e5ab9f9775a0fab
Manish kumar <manishkumar7543811190@gmail.com>
2022-08-18 02:47:20 +0530

    after secand

---
+++ src/CAR.java
@@ -0,0 +1,10 @@
+
+public class CAR {
+    public static void main(String[] args) {
+        Car_amnufacture honda=new Car_amnufacture();
+        honda.setModal("21_qjd");
+        System.out.println(honda.getModal());
+
+
+    }
+}
---
+++ src/Car_amnufacture.java
@@ -0,0 +1,18 @@
+public class Car_amnufacture {
+
+        private int model_no;
+        private String name;
+        private String colar;
+        private  String modal;
+        // seting the modal name that we take in car class
+        public void setModal(String modal) {
+            this.modal = modal;
+        }
+        // geting the modal name
+        public String getModal() {
+            return modal;
+        }
+
+
+
+}
